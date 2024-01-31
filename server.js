c3732de7254f9d202ba0d1da10d9058ebcb20480
sullivan-lconnor <sullivan.lconnor@gmail.com>
2024-01-30 22:28:37 -0500

    add ufw rules to readme

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3000;
+const port = 8080;
 const host = '0.0.0.0';
 
 // SQLite database setup
