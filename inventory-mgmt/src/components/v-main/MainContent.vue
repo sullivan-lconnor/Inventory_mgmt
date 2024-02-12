b627eb5cb607928cf589500cfb7e7663ae164646
Finn Howell <t.finn.howell@gmail.com>
2024-02-11 20:40:44 -0500

    fixed url import

--- inventory-mgmt/src/components/v-main/MainContent.vue
+++ inventory-mgmt/src/components/v-main/MainContent.vue
@@ -3,6 +3,7 @@
     <v-container fluid>
       <RequestItemWidget v-if="currentWidget === 'request-item'" />
       <SubmitItemWidget v-if="currentWidget === 'submit-item'" />
+      <!-- Pass uuid as a prop directly -->
       <UUIDItemRouter v-if="currentWidget === 'mainpage-item'" :uuid="uuid"></UUIDItemRouter>
       <QRUtility v-if="currentWidget === 'barcode-gen'"/>
     </v-container>
@@ -11,26 +12,28 @@
 
 <script>
 import RequestItemWidget from '@/components/db-request-widgets/RequestItemWidget.vue';
-import QRUtility from '@/components/barcode-utility/QRUtility.vue'
+import QRUtility from '@/components/barcode-utility/QRUtility.vue';
 import UUIDItemRouter from '@/components/db-request-widgets/UUIDItemRouter.vue';
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 
 export default {
   name: "MainContent",
+  props: {
+    // Receive UUID as a prop
+    uuid: String,
+  },
   components: {
     RequestItemWidget,
     QRUtility,
-    UUIDItemRouter
+    UUIDItemRouter,
   },
-  setup() {
+  setup(props) {
     const route = useRoute();
     // Use the route's name to determine the current widget
     const currentWidget = computed(() => route.name);
-    // Extract the UUID from the route parameters
-    const uuid = computed(() => route.params.uuid);
 
-    return { currentWidget, uuid };
+    return { currentWidget, uuid: props.uuid };
   }
 };
 </script>
